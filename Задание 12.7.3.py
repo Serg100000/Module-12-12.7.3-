e575f5d42ed9078e9a3011654f7f1feb754f0d47
Serg100000 <sergey_kuznecov@list.ru>
2022-04-03 17:36:12 +0300

    added new file

---
+++ Задание 12.7.3.py
@@ -0,0 +1,17 @@
+﻿money = int(input('Введите сумму вклада: '))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+ТКБ = int((per_cent['ТКБ'])*(money/100))
+СКБ = int((per_cent['СКБ'])*(money/100))
+ВТБ = int((per_cent['ВТБ'])*(money/100))
+СБЕР = int((per_cent['СБЕР'])*(money/100))
+
+deposit = [ТКБ, СКБ, ВТБ, СБЕР]
+print(deposit)
+
+d = {'TKB': 5.6, 'SKB': 5.9, 'VTB': 4.28, 'SBER': 4.0}
+max_val = max(d.values())
+print(max_val)
+
+i = int((max_val)*(money/100))
+print(i)
